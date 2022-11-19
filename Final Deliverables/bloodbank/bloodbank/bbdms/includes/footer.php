d97b08a7a3d39f0380d911ba134e75c1cf0022f4
Giri <113664995+Giri2002@users.noreply.github.com>
2022-11-19 20:50:22 +0530

    Add files via upload

---
+++ Final Deliverables/bloodbank/bloodbank/bbdms/includes/footer.php
@@ -0,0 +1,8 @@
+
+
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">"Donate  Youre BloodFor A Reason ,Let Reason To Be Life"<br>Team Id-PNT2022TMID32253 <br>..THANKS FOR VISITING US..</a></p>
+        </div>
+        <!-- /.container -->
+    </footer>
